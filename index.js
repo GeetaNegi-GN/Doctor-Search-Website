cab54c539ea320bc19dd00b03485f1f377da7381
Geeta Negi <geetanegi550@gmail.com>
2023-04-21 12:01:19 +0530

    final commit ,website completed

---
+++ index.js
@@ -0,0 +1,82 @@
+
+var home = document.getElementById("home");
+if (home) { 
+  home.addEventListener("click", function () {
+    var anchor = document.querySelector("[data-scroll-to='homeSection']");
+    if (anchor) {
+      anchor.scrollIntoView({ block: "start", behavior: "smooth" });
+    }
+  });
+}
+
+var services = document.getElementById("services");
+if (services) {
+  services.addEventListener("click", function () {
+    var anchor = document.querySelector("[data-scroll-to='serviceSection']");
+    if (anchor) {
+      anchor.scrollIntoView({ block: "start", behavior: "smooth" });
+    }
+  });
+}
+
+var aboutUs = document.getElementById("aboutUs");
+if (aboutUs) {
+  aboutUs.addEventListener("click", function () {
+    var anchor = document.querySelector("[data-scroll-to='aboutSection']");
+    if (anchor) {
+      anchor.scrollIntoView({ block: "start", behavior: "smooth" });
+    }
+  });
+}
+
+var diagnosicBox = document.getElementById("diagnosicBox");
+if (diagnosicBox) {
+  diagnosicBox.addEventListener("click", function () {
+    var anchor = document.querySelector(
+      "[data-scroll-to='serviceDiaTextContainer']"
+    );
+    if (anchor) {
+      anchor.scrollIntoView({ block: "start", behavior: "smooth" });
+    }
+  });
+}
+
+var appointmentBox = document.getElementById("appointmentBox");
+if (appointmentBox) {
+  appointmentBox.addEventListener("click", function () {
+    var anchor = document.querySelector("[data-scroll-to='contactText']");
+    if (anchor) {
+      anchor.scrollIntoView({ block: "start", behavior: "smooth" });
+    }
+  });
+}
+
+var aboutUs1 = document.getElementById("aboutUs1");
+if (aboutUs1) {
+  aboutUs1.addEventListener("click", function () {
+    var anchor = document.querySelector("[data-scroll-to='aboutSection']");
+    if (anchor) {
+      anchor.scrollIntoView({ block: "start", behavior: "smooth" });
+    }
+  });
+}
+
+var services1 = document.getElementById("services1");
+if (services1) {
+  services1.addEventListener("click", function () {
+    var anchor = document.querySelector("[data-scroll-to='serviceSection']");
+    if (anchor) {
+      anchor.scrollIntoView({ block: "start", behavior: "smooth" });
+    }
+  });
+}
+
+var homeLink = document.getElementById("homeLink");
+if (homeLink) {
+  homeLink.addEventListener("click", function () {
+    var anchor = document.querySelector("[data-scroll-to='homeSection']");
+    if (anchor) {
+      anchor.scrollIntoView({ block: "start", behavior: "smooth" });
+    }
+  });
+} 
---
+++ webservice.php
@@ -0,0 +1,82 @@
+
+<?php
+
+if(isset($_POST['search']) && isset($_POST['area'])) {
+    
+    $search_param = $_POST['search'];
+    $search_area = $_POST['area'];
+
+    if(empty($search_area) || empty($search_param)) {
+        $data["result"] = "False";
+        $data["Message"] = "Invalid search parameters";
+        echo json_encode($data, JSON_UNESCAPED_SLASHES);
+        exit;
+    }
+
+    if(!is_string($search_area)) {
+        $data["result"] = "False";
+        $data["Message"] = "Invalid search area format";
+        echo json_encode($data, JSON_UNESCAPED_SLASHES);
+        exit;
+    }
+
+
+    if(!is_string($search_param)) {
+        $data["result"] = "False";
+        $data["Message"] = "Invalid search parameter format";
+        echo json_encode($data, JSON_UNESCAPED_SLASHES);
+        exit;
+    }
+
+    $servername = "localhost"; 
+    $username = "root"; 
+    $password = ""; 
+    $dbname = "doctorfound"; 
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+//$sql = "SELECT * FROM 'doctor' WHERE DoctorLocation LIKE '%".$search_area."%' AND DoctorInformation LIKE '%".$search_param."%'";
+
+$sql = "SELECT * FROM doctor WHERE DoctorLocation = '".$search_area."' AND DoctorInformation = '".$search_param."'";
+
+
+$result = $conn->query($sql);
+
+    if($result->num_rows > 0)
+    {
+        $data = '<b class="serviceheading2">We found the following doctor in your area</b>';
+        $doctor_data = "";
+        while($row = $result->fetch_assoc()){
+        $doctorid = $row["ID"];
+        $doctorname = $row["DoctorName"];
+        $doctorimage = $row["DoctorImage"];
+        $doctorinfo = $row["DoctorInformation"];
+        $contact = $row["contactNo"];
+    
+      
+            $doctor_data = $doctor_data . "<div style='display: flex; align-items: center;'>
+            <img class='dctext1-icon' alt='' style='padding: 30px; margin: 20px 0px 0px 10px;position: relative' src='{$doctorimage}' />
+            <div style='font-size: 14px;'>
+              <p style='display: inline-block; margin-right: 10px;'>{$doctorname}</p>
+              <p style='display: inline-block; margin-right: 10px;'>{$doctorinfo}</p>
+              <p style='display: inline-block;'>{$contact}</p>
+            </div>
+          </div>";
+  
+                  
+         } 
+      }
+      else {
+       $data = '<b class="serviceheading2">No doctor found in your area</b>';
+       $doctor_data="";
+      }
+  
+  } else {
+  
+      $data = '<b class="serviceheading2">Bad Query</b>';
+      $doctor_data="";
+  }
+  $data = $data.$doctor_data;
+  echo $data;
+  
+  ?>
